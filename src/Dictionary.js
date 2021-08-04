6ddd17b2872fa392739a548ffc6806904e669bec
vannguyen0201 <48660155+vannguyen0201@users.noreply.github.com>
2021-08-04 20:35:03 +1000

    added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,20 @@
-import { tSObjectKeyword } from "@babel/types";
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(null);
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
+
     function wordSearch(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeyword(event){
@@ -17,9 +24,11 @@ export default function Dictionary() {
 
     return (
         <div className="dictionary">
+            <h1>Search for a word...</h1>
             <form onSubmit={wordSearch}>
                 <input type="search" autoFocus={true} onChange={handleKeyword}/>
             </form>
+            
         </div>
     );
 }
